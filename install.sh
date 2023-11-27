f39b4ae9c9a670df4ea3bce3ecf0ab548e65a7c0
wzk <45650342+ddk1246@users.noreply.github.com>
2023-11-27 23:45:01 +0800

    init

---
+++ install.sh
@@ -0,0 +1,28 @@
+echo $SHELL 
+
+# update
+sudo apt update
+
+# install zsh
+sudo apt install zsh nvim curl git -y
+
+# Change the default login shell to zsh
+chsh -s $(which zsh)
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install font
+sudo apt install powerline fonts-powerline -y
+
+# install powerlevel10k
+git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# install autojump
+sudo apt install autojump -y
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+mv ~/.zshrc ~/.zshrc.backup
+cp .zshrc ~/.zshrc
