239ae7d5b70e5295346fa3629e2e38eb26dc5291
wzk <45650342+ddk1246@users.noreply.github.com>
2023-11-28 16:10:17 +0800

    kk

--- install.sh
+++ install.sh
@@ -4,26 +4,21 @@ echo $SHELL
 sudo apt update
 
 # install zsh
-sudo apt install zsh curl git -y
+sudo apt install zsh curl git snap powerline fonts-powerline autojump -y
 sudo snap install nvim --classic
 
 # Change the default login shell to zsh
-chsh -s $(which zsh)
+chsh -s $(which zsh) $(whoami)
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# install font
-sudo apt install powerline fonts-powerline -y
-
-# install powerlevel10k
+# install powerlevel10k zsh-syntax-highlighting zsh-autosuggestions
 git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-# install autojump
-sudo apt install autojump -y
-
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
-mv ~/.zshrc ~/.zshrc.backup
+if [ -f ~/.zshrc ]; then
+    mv ~/.zshrc ~/.zshrc.backup
+fi
 cp .zshrc ~/.zshrc
