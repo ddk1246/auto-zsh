973de020c5cd8a64064b98a8d241e6448de5ebee
wzk <45650342+ddk1246@users.noreply.github.com>
2023-11-28 20:26:45 +0800

    update

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ CAT="[\e[1;37mATTENTION\e[0m]"
 CWR="[\e[1;35mWARNING\e[0m]"
 CAC="[\e[1;33mACTION\e[0m]"
 INSTLOG="install.log"
+RUNZSH=no
+KEEP_ZSHRC=yes
 
 echo -e "$CNT - Current Shell: $SHELL, it will be [\e[1;32mZSH\e[0m]"
 
@@ -26,14 +28,6 @@ echo -e "$CNT - Install zsh"
 # sudo apt install zsh curl git powerline fonts-powerline autojump -y
 echo -e "$COK - Finish install zsh\n"
 
-# copy custom config
-if [ -f ~/.zshrc ]; then
-    echo -e "$CWR Save current config [~/.zshrc] To [~/.zshrc.backup]"
-    mv ~/.zshrc ~/.zshrc.backup
-fi
-echo -e "$CAC - Copy custom .zshrc to ~/.zshrc"
-cp .zshrc ~/.zshrc
-
 # install oh-my-zsh
 echo -e "$CNT - Install oh-my-zsh\n$CWR - \e[1;35m Please exit zsh after install it !!! \e[0m"
 if sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"; then
@@ -50,4 +44,12 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &>> $INSTLOG
 echo -e "$COK - Finish clone powerlevel10k zsh-syntax-highlighting zsh-autosuggestions \n"
 
-# exec zsh -i -c "source ~/.zshrc"
+# copy custom config
+if [ -f ~/.zshrc ]; then
+    echo -e "$CWR Save current config [~/.zshrc] To [~/.zshrc.backup]"
+    mv ~/.zshrc ~/.zshrc.backup
+fi
+echo -e "$CAC - Copy custom .zshrc to ~/.zshrc"
+cp .zshrc ~/.zshrc
+
+# logout 
