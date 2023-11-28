3c21c089b3c10b7f577e79a81acc5d108f23c7d4
wzk <45650342+ddk1246@users.noreply.github.com>
2023-11-28 16:47:42 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,7 @@ echo $SHELL
 sudo apt update
 
 # install zsh
-sudo apt install zsh curl git snap powerline fonts-powerline autojump -y
-sudo snap install nvim --classic
+sudo apt install zsh curl git powerline fonts-powerline autojump -y
 
 # Change the default login shell to zsh
 chsh -s $(which zsh) $(whoami)
@@ -14,9 +13,9 @@ chsh -s $(which zsh) $(whoami)
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # install powerlevel10k zsh-syntax-highlighting zsh-autosuggestions
-git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone -f --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 if [ -f ~/.zshrc ]; then
     mv ~/.zshrc ~/.zshrc.backup
